40ad727b04f1120cba3cc38fa8b8f61c806a554c
unknown <you@example.com>
2013-12-12 09:03:46 +0100

    StartScene bijgewerkt

---
+++ PyramidPanic/PyramidPanic/GameScenes/StartScene/StartScene.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace PyramidPanic
+{
+    public class StartScene : IGameState
+    {
+        //Fields
+        private PyramidPanic game;
+
+        //Constructor
+        public StartScene(PyramidPanic game)
+        {
+            this.game = game;
+            this.Initialize();
+        }
+
+        //Initialize
+        public void Initialize()
+        {
+            this.LoadContent();
+        }
+
+
+        //LoadContent
+        public void LoadContent()
+        {
+
+        }
+
+
+        //Update
+        public void Update(GameTime gameTime)
+        {
+            if (Input.EdgeDetectKeyDown(Keys.Right))
+            {
+                this.game.GameState = this.game.PlayScene;
+            }
+        }
+
+        //Draw
+        public void Draw(GameTime gameTime)
+        {
+            this.game.GraphicsDevice.Clear(Color.Red);
+        }
+    }
+}
